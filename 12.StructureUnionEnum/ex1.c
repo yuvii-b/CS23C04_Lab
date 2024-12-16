e1703578c28075ffd3a16264c747f87f81b8b64b
yuvaraj <yuvaraj.codes@gmail.com>
2024-12-16 10:22:39 +0530

    update: ex1

--- 12.StructureUnionEnum/ex1.c
+++ 12.StructureUnionEnum/ex1.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
 
-typedef int INTEGER;
+typedef struct{
+    int value;
+}INTEGER;
+
+INTEGER calculate(INTEGER a, INTEGER b, char operator) {
+    INTEGER result;
+    switch (operator) {
+        case '+':
+            result.value = a.value + b.value;
+            break;
+        case '-':
+            result.value = a.value - b.value;
+            break;
+        case '*':
+            result.value = a.value * b.value;
+            break;
+        case '/':
+            if (b.value == 0) {
+                printf("Error: Division by zero!\n");
+                result.value = 0;
+            } else {
+                result.value = a.value / b.value;
+            }
+            break;
+        default:
+            printf("Error: Invalid operator '%c'\n", operator);
+            result.value = 0;
+            break;
+    }
+    return result;
+}
 
 int main(){
-    INTEGER a = 10;
-    INTEGER b = 20;
-    printf("Addition: %d\nSubtraction: %d\nMultiplication: %d\nDivision: %d\nModulus: %d", a + b, a - b, a * b, a / b, a % b);
+    INTEGER a, b, c;
+    a.value = 10;
+    b.value = 5;
+    c = calculate(a, b, '+');
+    printf("Addition: %d + %d = %d\n", a.value, b.value, c.value);
+    c = calculate(a, b, '-');
+    printf("Subtraction: %d - %d = %d\n", a.value, b.value, c.value);
+    c = calculate(a, b, '*');
+    printf("Multiply: %d * %d = %d\n", a.value, b.value, c.value);
+    c = calculate(a, b, '/');
+    printf("Divide: %d / %d = %d\n", a.value, b.value, c.value);
+    c = calculate(a, b, '%');
+    printf("Modulus: %d % %d = %d\n", a.value, b.value, c.value);
     return 0;
 }
