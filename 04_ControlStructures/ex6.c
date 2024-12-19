bf2b9188a3b3f0779d295f5c06172777ce736739
Yuvaraj <yuvaraj.codes@gmail.com>
2024-12-19 08:05:06 +0530

    update ex6

--- 04_ControlStructures/ex6.c
+++ 04_ControlStructures/ex6.c
@@ -4,7 +4,7 @@ int main(){
   int year;
   printf("Enter a year: ");
   scanf("%d", &year);
-  if(year % 4 == 0 && year % 400 == 0 || year % 100 != 0)
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	  printf("The year %d is a leap year\n", year);
   else
 	  printf("The year %d is not a leap year\n", year);
