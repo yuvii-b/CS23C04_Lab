8112ddc8d43693111a76418cc1aed72042eeeef9
Dharani2816 <sarandharan514@gmail.com>
2025-01-14 22:12:21 +0530

    Changes made

--- 05_Iteration/ex11.c
+++ 05_Iteration/ex11.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <math.h>
-
+#include<stdio.h>
+#include<math.h>
 int main(){
-	long int bin, dec, temp, i = 0;
-	printf("Enter a number in binary: ");
-	scanf("%ld", &bin);
-	temp = bin;
-	do{
-		int l = temp % 10;
-		dec += l * pow(2, i);
-		temp /= 10;
-		++i;
-	}while(temp!=0);
-	printf("%ld in decimal is %ld", bin, dec);
-	return 0;
+    long int bin,dec=0;
+    printf("Enter a number in binary form:");
+    scanf("%ld",&bin);
+    long int temp = bin,d=0;
+    while(bin != 0){
+        int c = bin % 10;
+         dec = dec + c*pow(2,d);
+         bin = bin / 10;
+         d++;
+    }
+    printf("Decimal equivalent of %ld is %ld ",temp,dec);
 }
